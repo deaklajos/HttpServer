856d60bf69d37675829c748090bee55f8c41bc6c
Deak Lajos <deak.lala@hotmail.com>
2019-05-07 14:03:11 +0200

    Basic image example works.

--- ResponseWorker.cpp
+++ ResponseWorker.cpp
@@ -20,36 +20,36 @@ void ResponseWorker::run()
         // If index.html
         if(request.contains("GET / "))
             fileName = "index.html";
+        if(request.contains("GET /w3schools.jpg"))
+            fileName = "w3schools.jpg";
     }
     else
     {
         socket.close();
         return;
     }
-
     QString responseHeader("HTTP/1.0 200 OK\n\n");
     QString errorResponse("HTTP/1.0 404 Not found\n\n<html>404 Not found</html>");
-    QString response = responseHeader;
+    QByteArray response = responseHeader.toUtf8();
 
     QFile file(fileName);
     if(file.exists())
     {
         if(file.open(QIODevice::ReadOnly))
         {
-            QString filestr(file.readAll());
-            response += filestr;
+            response.append(file.readAll());
         }
         else
         {
-            response = errorResponse;
+            response = errorResponse.toUtf8();
         }
     }
     else
     {
-        response = errorResponse;
+        response = errorResponse.toUtf8();
     }
 
-    socket.write(response.toUtf8());
+    socket.write(response);
     socket.flush();
     socket.waitForBytesWritten();
     socket.close();
