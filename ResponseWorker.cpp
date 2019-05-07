3af9936e4b5867a43f611e78ab5b88baea1feeca
Deak Lajos <deak.lala@hotmail.com>
2019-05-07 13:44:32 +0200

    Basic index.html response works.

--- ResponseWorker.cpp
+++ ResponseWorker.cpp
@@ -1,3 +1,5 @@
+#include <QFile>
+
 #include "ResponseWorker.h"
 
 ResponseWorker::ResponseWorker()
@@ -8,10 +10,46 @@ void ResponseWorker::run()
 {
     if(!socketDescriptor) return;
 
+    QString fileName;
     QTcpSocket socket;
     socket.setSocketDescriptor(socketDescriptor);
+    if(socket.waitForReadyRead(500))
+    {
+        QString request(socket.readAll());
 
-    socket.write("From server: hello world");
+        // If index.html
+        if(request.contains("GET / "))
+            fileName = "index.html";
+    }
+    else
+    {
+        socket.close();
+        return;
+    }
+
+    QString responseHeader("HTTP/1.0 200 OK\n\n");
+    QString errorResponse("HTTP/1.0 404 Not found\n\n<html>404 Not found</html>");
+    QString response = responseHeader;
+
+    QFile file(fileName);
+    if(file.exists())
+    {
+        if(file.open(QIODevice::ReadOnly))
+        {
+            QString filestr(file.readAll());
+            response += filestr;
+        }
+        else
+        {
+            response = errorResponse;
+        }
+    }
+    else
+    {
+        response = errorResponse;
+    }
+
+    socket.write(response.toUtf8());
     socket.flush();
     socket.waitForBytesWritten();
     socket.close();
