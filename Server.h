1fa6c1ffd66d09631c080e7a1d72983ccedc4170
Deak Lajos <deak.lala@hotmail.com>
2019-05-12 03:02:38 +0200

    Added line breaks to documentation.

--- Server.h
+++ Server.h
@@ -8,14 +8,14 @@
 /**
  * @brief HTTP server implementation.
  *
- * Privilege handling:
- * The following commands should be executed on the executable using this class:
- * chown root [name_of_executable]
- * chmod 6555 [name_of_executable]
+ * Privilege handling:<br>
+ * The following commands should be executed on the executable using this class:<br>
+ * chown root [name_of_executable]<br>
+ * chmod 6555 [name_of_executable]<br>
  * The reason behind theese extra modifications is that the server needs to run
  * with ruid = not_root_user_id, rgid = not_root_group_id, euid = root_user_id,
  * egid = root_group_id so that listen can be called on protected ports and
- * chroot can be called to create a jail for the process.
+ * chroot can be called to create a jail for the process.<br>
  * After theese procedures the privileges will be dropped.
  */
 class Server : public QTcpServer
@@ -39,9 +39,9 @@ public:
     /**
      * @brief Starts the server.
      *
-     * Checks if the privileges are correct.
-     * Checks for the php package.
-     * Creates the environment for the server.
+     * Checks if the privileges are correct.<br>
+     * Checks for the php package.<br>
+     * Creates the environment for the server.<br>
      * Starts listening on all network interfaces using the designated port.
      * @return Returns 0 if successful, othervise not 0.
      */
@@ -51,7 +51,7 @@ protected:
     /**
      * @brief Handels the incoming requests.
      *
-     * Overrides QTcpServer::incomingConnection().
+     * Overrides QTcpServer::incomingConnection().<br>
      * Starts the handling of a request on another thread.
      * @param socketDescriptor Native socket descriptor for the accepted connection.
      */
