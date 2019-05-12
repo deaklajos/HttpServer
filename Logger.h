e7add7a53c4a2b5d21b157eb518c63a7a286a11d
Deak Lajos <deak.lala@hotmail.com>
2019-05-12 14:46:58 +0200

    Doxygen comments added to Logger.h

--- Logger.h
+++ Logger.h
@@ -3,16 +3,35 @@
 
 #include <QString>
 
+/**
+ * @brief Singleton threadsafe class for verbose file logging.
+ */
 class Logger
 {
 public:
+    /**
+     * @brief Returns the singleton instance.
+     * @return Singleton instace.
+     */
     static Logger& getInstance()
     {
         static Logger instance;
         return instance;
     }
 
+    /**
+     * @brief Writes a log line into the log.log file.
+     *
+     * The log file is lazy initialized.<br>
+     * This fuction is thread safe.<br>
+     * The log line has the following signature:<br>
+     * [QtMsgType] [thread_id] time_in("dd-MM-yyyy HH:mm:ss:zzz") message
+     *
+     * @param type Represents the type of the message.
+     * @param message The message string to be logged.
+     */
     void Log(QtMsgType type, const QString& message);
+
 private:
     Logger() {}
 
